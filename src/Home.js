cfe74552fb613d3cf33b6028a1a79885da839542
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2022-09-21 21:21:19 +0530

    Added files

---
+++ src/Home.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+function Home() {
+  return (
+    <div className="app">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://m.media-amazon.com/images/S/sonata-images-prod/ACQ_HO_T1/89aa0cfb-43bf-4651-afd5-9ce43a831060._UR3000,600_.jpeg"
+          alt=""
+        />
+        <div className="home_row">
+          <Product
+            title="Harry Potter-The Philosphers Stone part one of the Harry Potter series
+          written by author J K Rownling"
+            price={29.9}
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSD2c1bz9
+          x3ogunbqj43GkdLCC7aDSgy0-q70hXjrZZARV4o_pcpzRmX1_BfgLafN9WRB3XoQKRcySGwg
+          hSzWWck6QnYmwobPbtk3vA1jAA6jn9x70VUsZr&usqp=CAY"
+            rating={5}
+          />
+          <Product
+            title="Apple - iphone 14 pro"
+            price={29.9}
+            image="https://media.wired.com/photos/631a469e40fe1e8870aa39c8/master/w_2560%2Cc_limit/How-to-Preorder-iPhone-14-and-iPhone-14-Pro-Gear.png"
+            rating={5}
+          />
+          {/*Product*/}
+        </div>
+        <div className="home_row">
+          <Product
+            title="Harry Potter-The Philosphers Stone part one of the Harry Potter series
+          written by author J K Rownling"
+            price={29.9}
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSD2c1bz9
+          x3ogunbqj43GkdLCC7aDSgy0-q70hXjrZZARV4o_pcpzRmX1_BfgLafN9WRB3XoQKRcySGwg
+          hSzWWck6QnYmwobPbtk3vA1jAA6jn9x70VUsZr&usqp=CAY"
+            rating={5}
+          />
+          <Product
+            title="Harry Potter-The Philosphers Stone part one of the Harry Potter series
+          written by author J K Rownling"
+            price={29.9}
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSD2c1bz9
+          x3ogunbqj43GkdLCC7aDSgy0-q70hXjrZZARV4o_pcpzRmX1_BfgLafN9WRB3XoQKRcySGwg
+          hSzWWck6QnYmwobPbtk3vA1jAA6jn9x70VUsZr&usqp=CAY"
+            rating={5}
+          />
+          <Product
+            title="Harry Potter-The Philosphers Stone part one of the Harry Potter series
+          written by author J K Rownling"
+            price={29.9}
+            image="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSD2c1bz9
+          x3ogunbqj43GkdLCC7aDSgy0-q70hXjrZZARV4o_pcpzRmX1_BfgLafN9WRB3XoQKRcySGwg
+          hSzWWck6QnYmwobPbtk3vA1jAA6jn9x70VUsZr&usqp=CAY"
+            rating={5}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            title="Harry Potter-The Philosphers Stone part one of the Harry Potter series
+          written by author J K Rownling"
+            price={29.9}
+            image="https://toshibatv-in.com/wp-content/uploads/2020/08/TO-L50-WB-AD-02.jpg"
+            rating={5}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
+const firebaseConfig = {
+  apiKey: "AIzaSyD7pwDUG-WvfaDivVPVM9WyLORa5-InuPE",
+  authDomain: "challange-c0e2b.firebaseapp.com",
+  projectId: "challange-c0e2b",
+  storageBucket: "challange-c0e2b.appspot.com",
+  messagingSenderId: "973265102142",
+  appId: "1:973265102142:web:1597e6e8f5c26fbbb8f86f",
+  measurementId: "G-WR4M8ZYM1C",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
